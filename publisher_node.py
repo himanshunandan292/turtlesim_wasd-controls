34cc859aa35ea2486e8ed817d946c749252e4af3
himanshunandan292 <69893139+himanshunandan292@users.noreply.github.com>
2021-01-02 22:37:31 +0530

    Update publisher_node.py

--- publisher_node.py
+++ publisher_node.py
@@ -1,7 +1,10 @@
-import rospy from geometry_msgs.msg import Twist
+import rospy 
+from geometry_msgs.msg 
+import Twist
 
 def talker():
-    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10) rospy.init_node('talker', anonymous=True) rate = rospy.Rate(10) # 10hz
+    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('talker', anonymous=True) rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         cmnd = raw_input()
         arg = Twist()
